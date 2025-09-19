a9d5916ba398a5e3e9fa7694c3fe3180cefaa189
Mason Carr <luffy42891@gmail.com>
2025-09-19 14:40:23 -0500

    added dummy stuff so build doesnt fail

--- ChargerAstronomyEngine/Data/CsvStarRepository.cs
+++ ChargerAstronomyEngine/Data/CsvStarRepository.cs
@@ -1,6 +1,11 @@
 ﻿
 
+using ChargerAstronomyShared.Contracts.Models;
 using ChargerAstronomyShared.Contracts.Repositories;
+using ChargerAstronomyShared.Contracts.Streaming;
+using ChargerAstronomyShared.Domain.Equatorial;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace ChargerAstronomyEngine.Data
@@ -8,5 +13,24 @@ namespace ChargerAstronomyEngine.Data
 
     public sealed class CsvStarRepository : IStarRepository
     {
+        public Task<PageResult<EquatorialStar>> GetAllAsync(PageRequest page)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<EquatorialStar> GetStarByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task ProducePagesAsync(IInitializationQueue<PageResult<EquatorialStar>> queue, PageRequest firstPage, CancellationToken cancellationToken = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PageResult<EquatorialStar>> QueryBySkyRegionAsync(SkyRegion skyRegion, PageRequest page)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
