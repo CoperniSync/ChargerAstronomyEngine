c539058e210bcf3cdeb7ae71f87a89a2a595669f
djoogway <theflaffywaffe@gmail.com>
2025-09-20 01:39:41 -0500

    basic tests using integers

--- tests/InitializationQueueTest.cs
+++ tests/InitializationQueueTest.cs
@@ -3,13 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xunit;
 using FluentAssertions;
 using System.Runtime.CompilerServices;
 
+using ChargerAstronomyEngine.Data;
+using ChargerAstronomyEngine.Streaming;
+using ChargerAstronomyShared.Contracts.Models;
+using ChargerAstronomyShared.Domain.Equatorial;
+
 namespace tests
 {
-    internal class InitializationQueueTest
+    public class InitializationQueueTest
     {
+        [Fact]
+        public void EnqueueBlocking_ShouldAddItemsToQueue()
+        {
+            // Arrange
+            var queue = new BoundedInitializationQueue<int>(5);
+
+            // Act
+            queue.EnqueueBlocking(1, CancellationToken.None);
+            queue.EnqueueBlocking(2, CancellationToken.None);
+
+            // Assert
+            queue.Count.Should().Be(2);
+        }
+
+        [Fact]
+        public void TryDequeue_ShouldRemoveItemsFromQueue()
+        {
+            // Arrange
+            var queue = new BoundedInitializationQueue<int>(5);
+            queue.EnqueueBlocking(1, CancellationToken.None);
+            queue.EnqueueBlocking(2, CancellationToken.None);
+
+            // Act
+            var success1 = queue.TryDequeue(out var item1);
+            var success2 = queue.TryDequeue(out var item2);
+
+            // Assert
+            success1.Should().BeTrue();
+            item1.Should().Be(1);
+            success2.Should().BeTrue();
+            item2.Should().Be(2);
+            queue.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void Complete_ShouldMarkQueueAsCompleted()
+        {
+            // Arrange
+            var queue = new BoundedInitializationQueue<int>(5);
+
+            // Act
+            queue.Complete();
+
+            // Assert
+            queue.IsCompleted.Should().BeTrue();
+        }
+
+        [Fact]
+        public void EnqueueBlocking_ShouldThrowWhenCapacityExceeded()
+        {
+            // Arrange
+            var queue = new BoundedInitializationQueue<int>(2);
+            queue.EnqueueBlocking(1, CancellationToken.None);
+            queue.EnqueueBlocking(2, CancellationToken.None);
+
+            // Act
+            Action act = () => queue.EnqueueBlocking(3, CancellationToken.None);
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Dispose_ShouldCompleteQueue()
+        {
+            // Arrange
+            var queue = new BoundedInitializationQueue<int>(5);
+
+            // Act
+            queue.Dispose();
+
+            // Assert
+            queue.IsCompleted.Should().BeTrue();
+        }
     }
 }
