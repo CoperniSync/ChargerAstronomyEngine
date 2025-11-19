08f5a13e2fbe9a1b76316fa21965d06837d53fa1
djoogway <theflaffywaffe@gmail.com>
2025-11-18 20:09:02 -0600

    Documentation update

--- ChargerAstronomyEngine/Streaming/EquatorialCalculator.cs
+++ ChargerAstronomyEngine/Streaming/EquatorialCalculator.cs
@@ -48,33 +48,46 @@ namespace ChargerAstronomyEngine.Streaming
             };
         }
 
+        /// <inheritdoc/>
         public void UpdateLocation(Observer newLocation)
         {
             location = newLocation;
         }
 
+        /// <inheritdoc/>
         public void UpdateTimeAndLocation(CalendarDateTime newTime, Observer newLocation)
         {
             astroTime = new AstroTime(newTime);
             location = newLocation;
         }
 
+        /// <inheritdoc/>
         public void UpdateTime(CalendarDateTime newTime)
         {
             astroTime = new AstroTime(newTime);
         }
 
+        /// <inheritdoc/>
         public void GetTimeAndPlace(out AstroTime time, out Observer location)
         {
             time = astroTime;
             location = this.location;
         }
 
+        /// <inheritdoc/>
         public void IncrementTime(float deltaTime)
         {
             astroTime = new AstroTime(astroTime.ut + deltaTime / 86400.0);
         }
 
+        /// <summary>
+        /// Updates the horizontal coordinates of the specified star based on the current time and location.
+        /// </summary>
+        /// <remarks>This method calculates the star's equatorial coordinates using the J2000 epoch and
+        /// applies corrections for aberration.  It then converts the equatorial coordinates to topocentric horizontal
+        /// coordinates, updating the star's altitude and azimuth.</remarks>
+        /// <param name="star">The star whose horizontal coordinates are to be updated. The star must have a valid <see cref="HorizontalBody"/>
+        /// property.</param>
         public void UpdateStar(T star)
         {
             var horizontal = star.HorizontalBody;
@@ -92,6 +105,11 @@ namespace ChargerAstronomyEngine.Streaming
             horizontal.Azimuth = topocentric.azimuth;
         }
 
+        /// <summary>
+        /// Updates the position and related properties of the specified planet based on the current astronomical time
+        /// and location.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown if the <paramref name="planet"/> name is not recognized in the internal collection of known planets.</exception>
         public void UpdatePositionOf(HorizontalPlanet planet)
         {
             if (planets.TryGetValue(planet.Name, out BodyType bodyType))
@@ -114,10 +132,15 @@ namespace ChargerAstronomyEngine.Streaming
             } else
             {
                 const string msg = "Planet name not recognized in UpdatePositionOf(HorizontalPlanet)";
-                throw new ArgumentException(msg, nameof(planet));
+                throw new ArgumentException(msg, planet.Name);
             }
         }
 
+        /// <summary>
+        /// Updates the position, phase, and distance of the specified moon object based on the current astronomical
+        /// time and location.
+        /// </summary>
+        /// <param name="moon">The <see cref="HorizontalMoon"/> object whose properties will be updated. Cannot be <see langword="null"/>.</param>
         public void UpdatePositionOf(HorizontalMoon moon)
         {
             var currentTime = this.astroTime;
@@ -138,6 +161,11 @@ namespace ChargerAstronomyEngine.Streaming
             moon.Distance = equatorial.dist;
         }
 
+        /// <summary>
+        /// Updates the position of the specified <see cref="HorizontalSun"/> object based on the current astronomical
+        /// time and location.
+        /// </summary>
+        /// <param name="sun">The <see cref="HorizontalSun"/> object whose altitude, azimuth, and distance will be updated.</param>
         public void UpdatePositionOf(HorizontalSun sun)
         {
             var currentTime = this.astroTime;
