042a8f332b708d0b957a03d11f598c7c28b732aa
djoogway <theflaffywaffe@gmail.com>
2025-11-30 16:45:23 -0600

    Fix script

--- Docs/render-build.sh
+++ Docs/render-build.sh
@@ -3,5 +3,4 @@
 wget https://github.com/dotnet/docfx/releases/download/v2.77.0/docfx-linux-x64-v2.77.0.zip
 unzip docfx-linux-x64-v2.77.0.zip -d docfx
 
-cd ./Docs
-./docfx/docfx build docfx.json
+. build docfx.json
